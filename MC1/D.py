61c210340ebccae8d120c2215397e01fbdf5fae7
jarint <jarin.thundathil@ucalgary.ca>
2025-02-07 15:48:06 -0700

    small change MC1

--- MC1/D.py
+++ MC1/D.py
@@ -6,13 +6,24 @@ import sys
 
 n, d = map(int, sys.stdin.readline().strip().split())
 nums = list(map(int, sys.stdin.readline().strip().split()))
+new_nums = [num // d for num in nums]
 
-pairs = []
-for i in range(len(nums)):
-    for j in range(i+1, len(nums)):
-        ai = nums[i]
-        aj = nums[j]
-        if (ai // d) == (aj // d):
-            pairs.append((ai, aj))
+#pairs = []
+#for i in range(len(nums)):
+#    for j in range(i+1, len(nums)):
+#        ai = nums[i]
+#        aj = nums[j]
+#        if (ai // d) == (aj // d):
+#            pairs.append((ai, aj))
+#
+#print(len(pairs))
 
-print(len(pairs))
+print(nums)
+print(new_nums)
+
+
+num_dict = {}
+for index, value in enumerate(nums):
+    num_dict.add(index, value)
+
+
