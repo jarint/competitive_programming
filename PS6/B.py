db215e7956a0189039dda3bc2b30d551ab1cf466
jarint <jarin.thundathil@ucalgary.ca>
2025-03-05 01:52:35 -0700

    problem B solved

--- PS6/B.py
+++ PS6/B.py
@@ -1,4 +1,29 @@
 import sys
 
+# input handling
+n = int(sys.stdin.readline().strip())
+weights = []
+for i in range(n):
+    weights.append(int(sys.stdin.readline().strip()))
 
+if n == 0:
+    print(0)
+    sys.exit()
 
+longest_increasing_sequence = [1] * n
+for i in range(n - 2, -1, -1):
+    for j in range(n - 1, i, -1):
+        if weights[j] > weights[i]:
+            longest_increasing_sequence[i] = max(longest_increasing_sequence[i], 1 + longest_increasing_sequence[j])
+
+longest_decreasing_sequence = [1] * n
+for i in range(n - 2, -1, -1):
+    for j in range(n - 1, i, -1):
+        if weights[j] < weights[i]:
+            longest_decreasing_sequence[i] = max(longest_decreasing_sequence[i], 1 + longest_decreasing_sequence[j])
+
+result = 0
+for i in range(n):
+    result = max(result, longest_increasing_sequence[i] + longest_decreasing_sequence[i] - 1)
+
+print(result)
